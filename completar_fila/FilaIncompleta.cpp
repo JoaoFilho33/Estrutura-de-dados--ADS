2d6bc022c472968e6f50da8abb38f8196282a2a1
João de Deus <joaod.almeidafi@gmail.com>
2022-09-28 00:47:44 -0300

    Completando Fila

--- completar_fila/FilaIncompleta.cpp
+++ completar_fila/FilaIncompleta.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 class No{
 	public:
-		char nome;
+		char item;
 		No *prox;
 		No(char n){
-			nome = n;
+			item = n;
 			prox = NULL;
 		}
 };
@@ -26,29 +26,73 @@ class Fila{
 		}
 		
 		void append(char n){
-			
+			if(inicio == NULL){
+				inicio = new No(n);
+				fim = inicio;
+			}
+			else{
+				No* newNo = new No(n);
+				fim->prox = newNo;
+				fim = newNo;
+			}
 		}
 		
 		char pop(){
-			
+			if(inicio != NULL) {
+				char item = inicio->item;
+				No* aux = inicio;
+				inicio = inicio->prox;
+				free(aux);
+				return item;
+			}
+			else {
+				return -1;
+			}
 		}
 		
 		bool isEmpty(){
 			return (inicio == NULL);
 		}
 	
-	    public void popAll(){
-	    	
+	    void popAll(){
+	    	while(!isEmpty()){
+				pop();
+			}
 	    	
 		}
 
 };
 
 int main(){
+	char item;
+	Fila* fila = new Fila;
 	
+    cout << "Insira 4 elementos\n";
 	
-    
-	
+	if(fila->isEmpty()){
+		for(int i = 0; i < 4; i++) {
+			cin >> item;
+			fila->append(item);
+		}
+	}
+	else{
+		cout << "Fila Cheia\n";
+	}
 
-	
+	cout << "Pop: " << fila->pop() << endl;
+
+	if(fila->isEmpty()) {
+		cout << "Fila vazia";
+
+	}
+
+	cout << "\npopAll\n\n";
+	fila->popAll();
+
+	if(fila->isEmpty()) {
+		cout << "Fila Vazia" << endl;
+	}
+	else{
+		cout << "Fila Cheia\n";
+	}
 }
