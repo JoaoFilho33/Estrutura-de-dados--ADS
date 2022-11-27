20ef31371baca8071c1916f6f321a3f62e4c51f3
João de Deus <joaod.almeidafi@gmail.com>
2022-11-27 15:45:14 -0300

    Quick Sort

---
+++ pesquisaEordenacao/quicksor_incompleto.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+void Quick(int vetor[10], int inicio, int fim) {
+   int i = inicio;
+   int j = fim;
+   int pivo = vetor[(i + j) / 2];
+   int temp;
+
+   while (i <= j) {
+      while (vetor[i] < pivo)
+         i++;
+      while (vetor[j] > pivo)
+         j--;
+      if (i <= j)
+      {
+         temp = vetor[i];
+         vetor[i] = vetor[j];
+         vetor[j] = temp;
+         i++;
+         j--;
+      }
+   }
+   if (j > inicio)
+      Quick(vetor, inicio, j);
+   if (i < fim)
+      Quick(vetor, i, fim);
+}
+
+int main(){
+   int vetor[6] = {7, 9, 4, 3,6,1};
+   int i;   
+   Quick(vetor, 0, 5);
+   printf("\n2.Vetor ordenado:\n");
+   for(i = 0; i <= 5; i++){
+      printf("%d ", vetor[i]);
+   }
+   printf("\n");   
+}
